7bb702288509276035cc071bec961124cb4968a3
v-yintia <67682398+v-yintia@users.noreply.github.com>
2024-06-17 05:27:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var skills = {
+        name: 'Naruto Uzumaki',
+        age: 32,
+        village: 'Konoha',
+        jutsu: 'Rasengan',
+        clan: 'Uzumaki',
+        rank: 'Hokage',
+        summon: 'Toad'
+    };
+    return skills;
+}
